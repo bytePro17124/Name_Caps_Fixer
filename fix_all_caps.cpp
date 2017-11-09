fba832243bb8e63a0125881eb9a738880f1b5dfa
Matthew Early <matthewjearly@gmail.com>
2017-11-09 18:31:05 -0500

    instruction update

--- fix_all_caps.cpp
+++ fix_all_caps.cpp
@@ -1,4 +1,6 @@
+// compile with ````g++ fix_all_caps.cpp -std=c++11````
 // run with ````./programname.out (string)filename (int)linecount````
+// example run command in terminal ````./a.out last_names.dat 1000````
 // changes each line to all lowercase, except the first letter which is capitalized.
 // great for line seperated list of names
 
